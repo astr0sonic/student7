0972f0e76c041d3b29223034ae7dbb88d8bae895
kgaraev_3@mail.ru <kgaraev_3@mail.ru>
2024-05-14 22:13:52 +0300

    task4

--- volume.cpp
+++ volume.cpp
@@ -3,16 +3,16 @@
 int getMaxVolume(int a, int b, int c, int n, int* arr) {
     int maxVolume = 0;
 
-    for (int i = 0; i < n - a; ++i) {
-        for (int j = i + a; j < n; ++j) {
-            int minHeight = std::min(arr[i], arr[j]);
-            if (minHeight <= c) {
-                int length = j - i;
-                int volume = b * length * minHeight;
-                maxVolume = std::max(maxVolume, volume);
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (j - i <= a && std::min(arr[i], arr[j]) <= c) {
+                
+                int volume = b * (j - i) * std::min(arr[i], arr[j]);
+                
+                maxVolume = (volume > maxVolume) ? volume : maxVolume;
             }
         }
     }
-    
+
     return maxVolume;
 }
